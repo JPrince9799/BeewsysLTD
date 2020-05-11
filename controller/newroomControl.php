e989f44a84aac970b99acc7674e1e68f58ffd2c9
JPrince9799 <joseph.agbodjan@ashesi.edu.gh>
2020-05-11 21:17:55 +0000

    add room Complete with validation

--- controller/newroomControl.php
+++ controller/newroomControl.php
@@ -5,29 +5,40 @@ require('session.php');
 
 if(isset($_POST['createRoom'])){
     $adminID = $_SESSION['id'];
+    
     $roomName = $_POST['roomName'];
     $lectureHall = $_POST['LHall'];
     $roomCapacity = $_POST['roomCapacity'];
     $rdate = $_POST['roomdate'];
     $startTime = $_POST['startTime'];
     $endTime = $_POST['endTime'];
+    
     $db = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
     $createRoom = "INSERT INTO `rooms` (`roomID`, `adminID`, `roomname`, `lecturehall`, `roomcapacity`, `roomdate`, `starttime`, `endtime`) 
     VALUES (NULL, '$adminID', '$roomName', '$lectureHall', '$roomCapacity', '$rdate', '$startTime', '$endTime');";
     
-    $result = $db->query($createRoom);
+    
 
-    if($result){
-        echo "<script> 
-                alert('Your room has been created!');
-                window.location.href ='../view/admin/newroom.php';
-                </script>";
+    if($roomName!="" && $lectureHall!="" && $roomCapacity!="" && $rdate!="" && $startTime!="" && $endTime!=""){
+        $result = $db->query($createRoom);
+        if($result){
+            echo "<script> 
+                    alert('Room created!');
+                    window.location.href ='../view/admin/newroom.php';
+                    </script>";
+        }
+        else{
+            echo "<script> 
+                    alert('Room has not been created!');
+                    window.location.href ='../view/admin/newroom.php';
+                    </script>";
+        }
     }
     else{
         echo "<script> 
-                alert('Your room has not been created!');
-                window.location.href ='../view/admin/newroom.php';
-                </script>";
+                    alert('Please make sure you have filled all the boxes!');
+                    window.location.href ='../view/admin/newroom.php';
+                    </script>";
     }
 
 }
